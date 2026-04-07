1756382e71561d9049197c0aaa1c5d7c76f8c1cc
HubertMRD <hubertdelia21@gmail.com>
2026-04-07 08:24:13 -0400

    Update ToDoViewModel.kt

--- app/src/main/java/com/example/todo_listapp/ToDoViewModel.kt
+++ app/src/main/java/com/example/todo_listapp/ToDoViewModel.kt
@@ -3,28 +3,45 @@ package com.example.todo_listapp
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 
-class ToDoViewModel: ViewModel() {
+class ToDoViewModel : ViewModel() {
 
     val taskList = mutableStateListOf<Task>()
 
-    fun populateTaskList(){
-        addTask( "Buy eggs")
-        addTask("buy milk")
-        addTask( "Buy suger")
+    fun populateTaskList() {
+        addTask("Buy eggs")
+        addTask("Buy milk")
+        addTask("Buy sugar")
         addTask("Buy flour")
     }
 
-    fun addTask(body: String){
+    fun addTask(body: String) {
         taskList.add(Task(body = body))
     }
 
-    fun deleteTask(task: Task){
+    fun deleteTask(task: Task) {
         taskList.remove(task)
     }
 
-    fun toggleTaskCompleted(task: Task){
+    fun toggleTaskCompleted(task: Task) {
         val index = taskList.indexOf(task)
         taskList[index] = taskList[index].copy(completed = !task.completed)
     }
 
+    // NEW: Uncheck all tasks
+    fun uncheckAll() {
+        for (i in taskList.indices) {
+            taskList[i] = taskList[i].copy(completed = false)
+        }
+    }
+
+    // NEW: Delete all completed tasks
+    fun deleteCompleted() {
+        taskList.removeAll { it.completed }
+    }
+
+    // NEW: Move task to bottom
+    fun moveToBottom(task: Task) {
+        taskList.remove(task)
+        taskList.add(task.copy()) // copy to avoid key collision
+    }
 }
