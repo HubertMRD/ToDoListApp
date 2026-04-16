15934c6deee4c92654ff7dd97ac463c3a5d358f3
HubertMRD <hubertdelia21@gmail.com>
2026-04-16 08:56:50 -0400

    Update Task.kt

--- app/src/main/java/com/example/todo_listapp/Task.kt
+++ app/src/main/java/com/example/todo_listapp/Task.kt
@@ -3,9 +3,11 @@ package com.example.todo_listapp
 import java.util.UUID
 
 data class Task(
-    var id: UUID= UUID.randomUUID(),
+    var id: UUID = UUID.randomUUID(),
     var body: String = "",
     var completed: Boolean = false
-)
-
-
+) {
+    companion object {
+        fun newId(): UUID = UUID.randomUUID()
+    }
+}
