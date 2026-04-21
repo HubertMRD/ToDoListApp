99982006c024985a2f2d0f360ed19cdf9dad8c5c
HubertMRD <hubertdelia21@gmail.com>
2026-04-21 10:49:23 -0400

    Update Task.kt

--- app/src/main/java/com/example/todo_listapp/Task.kt
+++ app/src/main/java/com/example/todo_listapp/Task.kt
@@ -11,3 +11,36 @@ data class Task(
         fun newId(): UUID = UUID.randomUUID()
     }
 }
+_________________
+package com.example.todo_listapp
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TaskCard(
+    task: Task,
+    toggleCompleted: (Task) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .padding(8.dp)
+            .clickable { toggleCompleted(task) }
+    ) {
+        Checkbox(
+            checked = task.completed,
+            onCheckedChange = { toggleCompleted(task) }
+        )
+        Text(
+            text = task.body,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
